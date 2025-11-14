b163efceef313dca8f0a1ed8c126651eb6375f1e
Joseph Ebuka <ebukaj665@gmail.com>
2025-11-14 23:55:42 +0100

    Switch to my own mongo instance

--- Helpers/database/connectDatabase.js
+++ Helpers/database/connectDatabase.js
@@ -1,8 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-connectDatabase = async  () => {
-    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.q3pgg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0` ,{useNewUrlParser : true})
-    console.log("MongoDB Connection Successfully")
-}
+connectDatabase = async () => {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lyio3vv.mongodb.net/?appName=Cluster0`,
+    {
+      useNewUrlParser: true,
+      // serverSelectionTimeoutMS: 5000, // Timeout after 5s
+      // connectTimeoutMS: 10000, // Give up initial connection after 10s
+    },
+  );
+  console.log("MongoDB Connection Successfully");
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
